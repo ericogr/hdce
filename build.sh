6e0be67380b2b45a76c0288416a0c4e28851109b
EricoGR <ericogr@gmail.com>
2020-07-23 22:40:57 -0300

    primeira versao do docker config encoder

---
+++ build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+package="bitbucket.org/ericogr/hdce"
+package_split=(${package//\// })
+package_name=${package_split[-1]}
+platforms=("windows/amd64" "linux/amd64")
+output_dir=bin
+ldflags="-s -w"
+upxflags="-q"
+
+#install common dependencies
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$package_name'-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    echo -n "building $output_dir/$output_name: "
+    env GOOS=$GOOS GOARCH=$GOARCH go build -ldflags="$ldflags" -o $output_dir/$output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred (build)! Aborting the script execution...'
+        exit 1
+    fi
+    echo "ok"
+
+    echo -n "compressing $output_dir/$output_name: "
+    upx $upxflags $output_dir/$output_name>/dev/null
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred (upx)! Aborting the script execution...'
+        echo '>>> Is upx installed? <<<'
+        exit 1
+    fi
+    echo "ok"
+    echo
+done
---
+++ hdce.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "fmt"
+  "encoding/base64"
+)
+
+func main() {
+  var usuarioHarbor string
+  var senhaHarbor string
+
+  fmt.Printf("Digite o usuário harbor: ")
+  fmt.Scanln(&usuarioHarbor)
+
+  fmt.Printf("Digite a senha harbor: ")
+  fmt.Scanln(&senhaHarbor)
+
+  fmt.Println("\n" + encodingDockerConfig(usuarioHarbor, senhaHarbor))
+}
+
+func encodingDockerConfig(usuarioHarbor string, senhaHarbor string) (encodedDockerConfig string) {
+  return encoderBase64(templateDockerConfig(encoderBase64(concatenaUsuarioSenha(usuarioHarbor, senhaHarbor))))
+}
+
+func templateDockerConfig(auth string) (dockerConfig string){
+  dockerConfig = "{\"auths\":{\"https://docker.totvs.io/v1/\":{\"auth\":\"" + auth + "\"}}}"
+  return
+}
+
+func concatenaUsuarioSenha(usuario string, senha string) (concatenado string) {
+  concatenado = usuario + ":" + senha
+  return
+}
+
+func encoderBase64(texto string) (codificado string) {
+  data := []byte(texto)
+  codificado = base64.StdEncoding.EncodeToString(data)
+  return
+}
